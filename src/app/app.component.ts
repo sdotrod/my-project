b30394f6414c73ae95b93f7df68bdf995ad938ac
sandra rodriguez <sdot@sandras-MacBook-Air.local>
2017-10-12 14:42:01 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public postList;
+  title = 'WTT';
+
+  constructor(
+    private http: HttpClient,
+  ) {}
+
+  ngOnInit() {
+    this.http.get<Post>('https://www.reddit.com/r/php/search.json?q=cats&limit=5')
+      .subscribe(result => {
+        this.postList = result.data.children;
+        console.log('this.postList', this.postList);
+        // this.postList.forEach(post => {})
+      });
+  }
+}
+
+export class Post {
+  kind: string;
+  data: PostData;
+}
+
+export class PostData {
+  children: PostChildren[];
+}
+
+export class PostChildren {
+  data: PostChildrenData;
+}
+
+export class PostChildrenData {
+
 }
